ebcc72dbb99c9a21cad75c2d0da6e2cf70e80739
Praveen Kulkarni <praveen_kulkarni@apple.com>
2019-06-03 00:31:47 -0700

    feature branch commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and deploy to AWS..awesome!!
         </a>
       </header>
     </div>
